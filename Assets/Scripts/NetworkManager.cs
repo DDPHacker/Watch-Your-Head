892365fe0b16ba2bbebd3b600989cd395dae2c5e
Armour <497052684@qq.com>
2016-02-28 00:05:17 -0800

    updated UI Canvas

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
@@ -6,33 +7,63 @@ public class NetworkManager : MonoBehaviour {
 	[SerializeField] public GameObject playerPrefab;
 	[SerializeField] public Transform[] spawnPoints;
 
+	private GameObject UIWindow;
+	private GameObject loginWindow;
+	private InputField usernameInput;
+	private InputField roomNameInput;
+	private InputField roomListInput;
+	private Text loadingText;
 	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
+		UIWindow = GameObject.FindGameObjectWithTag("UICanvas");
+		loginWindow = UIWindow.transform.FindChild("Login").gameObject;
+		loadingText = UIWindow.transform.FindChild("Loading Text").GetComponent<Text>();
+		usernameInput = loginWindow.transform.FindChild("Username Input").GetComponent<InputField>();
+		roomNameInput = loginWindow.transform.FindChild("Roomname Input").GetComponent<InputField>();
+		roomListInput = loginWindow.transform.FindChild("Roomname List").GetComponent<InputField>();
 		PhotonNetwork.logLevel = PhotonLogLevel.Full;
-		PhotonNetwork.ConnectUsingSettings("v0.1");
+		PhotonNetwork.ConnectUsingSettings("v0.2");
+		StartCoroutine(UpdateStateText());
+	}
+
+	IEnumerator UpdateStateText() {
+		while (true) {
+			loadingText.text = PhotonNetwork.connectionStateDetailed.ToString();
+			yield return null;
+		}
 	}
 
 	void OnJoinedLobby() {
 		Debug.Log("On joined lobby!!");
-		JoinRoom();
+		loginWindow.SetActive(true);
+	}
+		
+	void OnReceivedRoomListUpdate() {
+		Debug.Log("On Received Room List!!");
+		roomListInput.text = "";
+		RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+		foreach (RoomInfo room in rooms)
+			roomListInput.text += room.name + "\n";
 	}
 
-	void JoinRoom() {
+	public void JoinRoom() {
 		Debug.Log("Join Room!!");
 		RoomOptions roomOption = new RoomOptions() {isVisible = true, isOpen = true, maxPlayers = 4};
-		PhotonNetwork.JoinOrCreateRoom("Test", roomOption, TypedLobby.Default);
+		PhotonNetwork.JoinOrCreateRoom(roomNameInput.text, roomOption, TypedLobby.Default);
 	}
 
 	void OnJoinedRoom() {
 		Debug.Log("On Joined Room!!!");
+		PhotonNetwork.player.name = usernameInput.text;
+		StopCoroutine(UpdateStateText());
+		UIWindow.SetActive(false);
 		StartCoroutine(SpawnPlayer());
 	}
 
 	IEnumerator SpawnPlayer() {
 		yield return new WaitForSeconds(0.0f);
-		//messageWindow.SetActive(true);
 		int spawnIndex = Random.Range(0, spawnPoints.Length);
 		player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation, 0);
 	}
