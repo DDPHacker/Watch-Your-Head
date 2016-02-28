28b9af3376d6d80c56c071dc8fc9655cd91fcc8d
ruochen_j <ruochenj@sfu.ca>
2016-02-27 17:01:34 -0800

    tile framwork

---
+++ Assets/Scripts/TrapTile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTile : GameTile {
+
+	// Use this for initialization
+	void Start () {
+		type = 5;
+		isFall = false;
+	}
+	override void stepOn(GameObject Player)
+	{
+		if (isFall == false) {
+			//damage
+			isFall = true;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WoodTile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoodTile : GameTile {
+
+	// Use this for initialization
+	void Start () {
+		type = 6;
+		isFall = true;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
